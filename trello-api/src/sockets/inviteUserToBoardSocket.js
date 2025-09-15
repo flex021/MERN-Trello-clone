91c56ba788508a7889bc199dafff0d60327efe57
flex021 <congthong2015@gmail.com>
2025-09-15 23:57:47 +0700

    Remove old frontend files

--- trello-api/src/sockets/inviteUserToBoardSocket.js
+++
@@ -1,7 +0,0 @@
-
-export const inviteUserToBoardSocket = (socket) => {
-  socket.on('FE_USER_INVITED_TO_BOARD', (invitation) => {
-
-    socket.broadcast.emit('BE_USER_INVITED_TO_BOARD', invitation)
-  })
-}
--- trello-web/src/socketClient.js
+++
@@ -1,4 +0,0 @@
-import { io } from 'socket.io-client'
-import { API_ROOT } from '~/utils/constants.js'
-
-export const socketIoInstance = io(API_ROOT)

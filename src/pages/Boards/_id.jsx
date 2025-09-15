291dc51f5f420061f8a3eb2491560be377d1fc33
flex021 <congthong2015@gmail.com>
2025-09-16 00:00:58 +0700

    Squashed 'trello-web/' content from commit 7dea87f
    git-subtree-dir: trello-web
    git-subtree-split: 7dea87fc3aeedfd4c211d75ea97e449085ab3992

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,95 @@
+import Container from '@mui/material/Container'
+import AppBar from '~/components/AppBar/AppBar.jsx'
+import BoardBar from './BoardBar/BoardBar.jsx'
+import BoardContent from './BoardContent/BoardContent.jsx'
+import { useEffect } from 'react'
+import {
+  moveCardToDifferentColumnAPI,
+  updateBoardDetailsAPI,
+  updateColumnDetailsAPI
+} from '~/apis/index.js'
+import { cloneDeep } from 'lodash'
+import {
+  fetchBoardDetailsAPI,
+  updateCurrentActiveBoard,
+  selectCurrentActiveBoard
+} from '~/redux/activeBoard/activeBoardSlice.js'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import PageLoadingSpinner from '~/components/Loading/PageLoadingSpinner.jsx'
+import ActiveCard from '~/components/Modal/ActiveCard/ActiveCard.jsx'
+
+
+function Board() {
+  const dispatch = useDispatch()
+  const board = useSelector(selectCurrentActiveBoard)
+
+  const { boardId } = useParams()
+
+  useEffect(() => {
+    dispatch(fetchBoardDetailsAPI(boardId))
+  }, [dispatch, boardId])
+
+
+  const moveColumns = async (dndOrderedColumns) => {
+    const dndOrderedColumnIds = dndOrderedColumns.map(c => c._id)
+    const newBoard = { ...board }
+    newBoard.columns = dndOrderedColumns
+    newBoard.columnOrderIds = dndOrderedColumnIds
+    dispatch(updateCurrentActiveBoard(newBoard))
+
+    await updateBoardDetailsAPI(newBoard._id, { columnOrderIds: dndOrderedColumnIds })
+  }
+
+  const moveCardInTheSameColumn = (dndOrderedCards, dndOrderedCardIds, columnId) => {
+    const newBoard = cloneDeep(board)
+    const columnToUpdate = newBoard.columns.find(column => column._id === columnId)
+    if ( columnToUpdate ) {
+      columnToUpdate.cards = dndOrderedCards
+      columnToUpdate.cardOrderIds = dndOrderedCardIds
+    }
+    dispatch(updateCurrentActiveBoard(newBoard))
+
+    updateColumnDetailsAPI(columnId, { cardOrderIds: dndOrderedCardIds })
+  }
+
+  const moveCardToDifferentColumn = (currentCardId, prevColumnId, nextColumnId, dndOrderedColumns) => {
+    const dndOrderedColumnIds = dndOrderedColumns.map(c => c._id)
+    const newBoard = { ...board }
+    newBoard.columns = dndOrderedColumns
+    newBoard.columnOrderIds = dndOrderedColumnIds
+    dispatch(updateCurrentActiveBoard(newBoard))
+
+    let prevCardOrderIds = dndOrderedColumns.find(c => c._id === prevColumnId)?.cardOrderIds
+    if (prevCardOrderIds[0].includes('placeholder-card')) prevCardOrderIds = []
+
+    moveCardToDifferentColumnAPI({
+      currentCardId,
+      prevColumnId,
+      prevCardOrderIds,
+      nextColumnId,
+      nextCardOrderIds: dndOrderedColumns.find(c => c._id === nextColumnId)?.cardOrderIds
+    })
+  }
+
+
+  if (!board) {
+    return <PageLoadingSpinner caption="Loading Board..." />
+  }
+
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <ActiveCard />
+      <AppBar />
+      <BoardBar board={board}/>
+      <BoardContent
+        board={board}
+        moveColumns = {moveColumns}
+        moveCardInTheSameColumn = {moveCardInTheSameColumn}
+        moveCardToDifferentColumn = {moveCardToDifferentColumn}
+      />
+    </Container>
+  )
+}
+
+export default Board
---
+++ src/theme.js
@@ -0,0 +1,79 @@
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
+
+const theme = extendTheme({
+  trello: {
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    columnHeaderHeight: COLUMN_HEADER_HEIGHT,
+    columnFooterHeight: COLUMN_FOOTER_HEIGHT
+  },
+  colorSchemes: {
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: 'white',
+            borderRadius: '8px'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) =>({
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          fontSize: '0.875rem',
+          '& fieldset': {
+            borderWidth: '1px !important'
+          },
+          '&:hover fieldset': {
+            borderWidth: '2px !important'
+          },
+          '&.Mui-focused fieldset': {
+            borderWidth: '2px !important'
+          }
+
+        })
+      }
+    }
+  }
+})
+
+export default theme
